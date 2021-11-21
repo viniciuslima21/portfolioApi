e8eebb034693a17633b8f07f63dbd2257d9687c6
viniciuslima21 <viniciuslimaandre@hotmail.com>
2021-11-21 15:33:14 -0300

    organizing index

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const app = express()
-app.use('/images', express.static('src/uploads'))
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 const routes = require('./routes/routes')
 app.use('/', routes)
+app.use('/images', express.static('src/uploads'))
 
 app.listen(process.env.PORT || 3333, () => {
     console.log('API Online!')
