e5c0b6668b6eee8450d6f994e06dc96b204a4235
Vinícius Lima <63244193+viniciuslimaan@users.noreply.github.com>
2023-05-01 12:40:26 -0300

    refactor: change mongodb connection message

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,15 @@
 const mongoose = require('mongoose')
 
 const url = process.env.MONGO_URL || 'mongodb://localhost:27017/portfolio'
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(
+    url,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (err) => {
+        if (err) return console.log(err)
+
+        console.log("Database connected!")
+        console.log("Mongo URL: " + process.env.MONGO_URL)
+    }
+)
 
 module.exports = mongoose
